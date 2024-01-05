2534821dfcbd6c87ac0e375b22fc04d124367d01
mmulyana <mulyan.t20@gmail.com>
2024-01-06 00:00:00 +0700

    update: change to common.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-import express from 'express'
+const express = require('express')
+const db = require('./utils/db')
+const path = require('path')
 
 const app = express()
 
-app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.listen(8000, () =>
-  console.log(`server is running in http://localhost:8000`)
+  console.log('server is running in http://localhost:8000')
 )
